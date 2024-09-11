b6dd1d8d9f0782298031054f36bff156f9de0020
Merten Popp <webmaster@merten-popp.de>
2024-09-11 21:11:21 +0200

    Increasing chance for new office from 20 to 30 (old chance)

--- src/Sim.cpp
+++ src/Sim.cpp
@@ -4162,7 +4162,7 @@ void COptions::ReadOptions() {
             OptionTicketPriceIncrement = 10;
         }
         if (!reg.ReadRegistryKey_u(OptionRentOfficeTriggerPercent)) {
-            OptionRentOfficeTriggerPercent = 20;
+            OptionRentOfficeTriggerPercent = 30;
         }
         if (!reg.ReadRegistryKey_u(OptionRentOfficeMinAvailable)) {
             OptionRentOfficeMinAvailable = 0;
