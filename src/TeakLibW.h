c06cbdd383feeca429bf94e942f26cc84eae4980
Merten Popp <webmaster@merten-popp.de>
2024-09-24 19:59:58 +0200

    Fix includes for WIN32
    Update toolset to v143

--- src/TeakLibW.h
+++ src/TeakLibW.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "defines.h"
+
+#include "SDL_log.h"
+#include "SDL_pixels.h"
+#include "SDL_rwops.h"
+#include "SDL_surface.h"
+
 #include <vector>
 
 extern void memswap(void *, void *, ULONG);
@@ -35,6 +42,9 @@ extern unsigned char GerToUpper(unsigned char);
 extern UINT KeycodeToUpper(UINT nChar);
 extern void RecapizalizeString(CString &str);
 extern const char *GetSuffix(const char *);
+
+#undef min
+#undef max
 
 #if defined(__RESHARPER__)
 #define PRINTF_ATTR(StringIndex, FirstToCheck) [[gnu::format(printf, StringIndex, FirstToCheck)]]
