77db4b912a4f416501c38d009037e78eb4a9a27c
Merten Popp <webmaster@merten-popp.de>
2026-09-17 18:33:30 +0200

    Revome additional args from run_competition.sh
    300 runs:
    59       FL  freegame  4.039232e+07  3.327306e+07 -8.264863e+06 -297.266667
    59       HA  freegame  2.208631e+09  1.921632e+09 -9.057327e+07  610.496667
    59       PT  freegame  3.711124e+07  3.335269e+07 -5.197580e+06 -257.590000
    59       SA  freegame -5.880007e+05  1.094428e+07  0.000000e+00    0.000000
    
    [240 rows x 6 columns]
    Day 59 / SaldoGesamt / Airline HA:  2208631285.0
    Day 59 / Firmenwert / Airline HA:  1921632234.7133334

--- scripts/run_competition.sh
+++ scripts/run_competition.sh
@@ -5,5 +5,5 @@ cd "/media/LINUX/GOG Games/Airline Tycoon Deluxe/game/"
 rm -rf dataCOMPETITION_*.csv
 rm -rf dataCOMPETITION_*.txt
 ruby threadpool.rb --prefix=dataCOMPETITION "/setbotlevel 24"
-python concat.py 'dataCOMPETITION_*.csv' HA,PT SaldoGesamt,Firmenwert
+python concat.py 'dataCOMPETITION_*.csv'
 cd -
